36caf75d78581d4bd2b0b43e6c5bad7067eb54e6
Mounecaike <jordanl4213@gmail.com>
2025-01-02 19:50:46 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -40,7 +40,7 @@ function createSkillsFromJSON() {
                 card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./assets/images/${item.image}" />
+                            <img src="../assets/images/${item.image}" />
                             <h4 class="card-title mt-3">${item.title}</h4>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
@@ -75,7 +75,7 @@ function createPortfolioFromJSON() {
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="../src/assets/images/${item.image}" style="width:100%">
+                    <img class="card-img-top" src="../assets/images/${item.image}" style="width:100%">
                     <div class="card-body">
                         <h4 class="card-title">${item.title}</h4>
                         <p class="card-text">${item.text}</p>
